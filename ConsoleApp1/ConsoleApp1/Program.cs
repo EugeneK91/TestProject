fa20f6d6ced598c09750c56e5f2bdf36724b7d03
EugeneK91 <eugene.a.korban@gmail.com>
2020-02-04 22:10:56 +0300

    check3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello world1");
-			Console.WriteLine("Hello world1");
+			Console.WriteLine("Hello world2");
 		}
 	}
 }
